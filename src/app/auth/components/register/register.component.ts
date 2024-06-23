f829301d37d2d8ae551c6b64d557ae38fa175534
mouloud78 <mouloudouldali78@gmail.com>
2024-06-23 16:08:12 -0400

    add register component, store and shared interfaces

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { register } from '../../store/action';
+import { RegisterRequestInterface } from '../../types/registerRequest.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
 })
 export class RegisterComponent {
   form = this.fb.nonNullable.group({
@@ -13,9 +17,13 @@ export class RegisterComponent {
     email: ['', Validators.required],
     password: ['', Validators.required],
   });
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private store: Store) {}
 
   onSubmit() {
     console.log('form', this.form.getRawValue());
+    const request: RegisterRequestInterface = {
+      user: this.form.getRawValue(),
+    };
+    this.store.dispatch(register({ request }));
   }
 }
